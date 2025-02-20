16161ff3e736819da83f36d5d337b447a3f83839
keven-bardales <keven.bardales@gmail.com>
2025-02-20 17:01:19 -0600

    feat: add new components in template

---
+++ src/modules/shared/presentation/components/framer/framer-wrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { ReactHTML } from "react";
+import { HTMLMotionProps, motion } from "framer-motion";
+import dynamic from "next/dynamic";
+
+export type TagNameType = keyof ReactHTML;
+
+export type MotionComponentProps<TagName extends TagNameType> = HTMLMotionProps<TagName>;
+
+export interface FramerProps<TagName extends keyof ReactHTML> {
+  as: TagName;
+  children?: React.ReactNode;
+  props?: MotionComponentProps<TagName>;
+}
+
+function Framer<TagName extends keyof ReactHTML>({ as, children, ...props }: FramerProps<TagName>) {
+  const MotionComponent = motion[as as keyof typeof motion];
+
+  return <MotionComponent {...props}>{children}</MotionComponent>;
+}
+
+export default Framer;
