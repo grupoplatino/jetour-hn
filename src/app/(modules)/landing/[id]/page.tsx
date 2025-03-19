3f689b57a8161e4127384fd7c791fb63288ef673
Edy-CM <138315223+Edy-CM@users.noreply.github.com>
2025-03-18 21:09:24 -0600

    feat:finished dynamic model render

---
+++ src/app/(modules)/landing/[id]/page.tsx
@@ -0,0 +1,188 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import Image from "next/image";
+import { NavBar } from "@/components/ui/navbar";
+import ContactForm from "@/components/ui/cotizacion";
+import Viewer3D from "@/components/ui/landing/viewer-section";
+import { SpecsSection } from "@/components/ui/landing/specs-section";
+import { CarSpects } from "@/components/ui/landing/car-specs";
+import { CarGallerySection } from "@/components/ui/landing/car-gallery-section";
+import { SinglePictureSection } from "@/components/ui/landing/single-picture-section";
+import { InformativeGallery } from "@/components/ui/landing/informative-gallery";
+import { InformativeCard } from "@/components/ui/landing/informative-card";
+import { WideImage } from "@/components/ui/landing/wide-image";
+import { VideoPlayer } from "@/components/ui/landing/video-player";
+import { vehicleData } from "../../../../../vehicle-data";
+import {
+  BenchmarkRow,
+  BenchMarkSection,
+  CarColorSection,
+} from "@/components/ui/landing/benchmarks-section";
+import {
+  CarBenchmarks,
+  CarColor,
+} from "@/components/ui/landing/car-benchmarks";
+import { HeroSection } from "@/components/ui/landing/hero";
+import ColorContext from "@/components/ui/color-context";
+import { SlicedWideImage } from "@/components/ui/landing/sliced-wide-image";
+import { Footer } from "@/components/ui/footer";
+import { useParams } from "next/navigation";
+
+export default function LandingPage() {
+  const CarPlaceholder = ({
+    image,
+    customClass,
+  }: {
+    image: string;
+    customClass: string;
+  }) => {
+    return (
+      <Image
+        src={image}
+        width={400}
+        height={400}
+        alt="Car Placeholder"
+        className={`absolute ${customClass}`}
+        style={{ transform: "translate(-50%, -50%)" }}
+      />
+    );
+  };
+
+  const params = useParams();
+  const carIds: Record<string, number> = {
+    dashing: 0,
+    x50: 1,
+    t2: 2,
+    x70: 3,
+  };
+
+  const id = Array.isArray(params.id) ? params.id[0] : params.id;
+
+  const carData = vehicleData[carIds[id ?? ""] ?? 0] as any;
+
+  return (
+    <>
+      <ColorContext.Provider value={carData.primaryColor}>
+        <NavBar />
+      </ColorContext.Provider>
+      <HeroSection
+        image={carData.hero.backgroundImage}
+        title={carData.hero.title}
+        subtitle={carData.hero.subTitle}
+      />
+
+      <BenchMarkSection>
+        <CarPlaceholder
+          image={carData.carModels[0]}
+          customClass="lg:top-[60%] top-[50%] z-10 left-[50%] md:h-[25rem] md:w-[45rem]"
+        />
+        <CarPlaceholder
+          image={carData.carModels[1]}
+          customClass="md:h-[16rem] md:w-[30rem] top-[40%] left-[15%] hidden lg:block"
+        />
+        {carData.carModels[2] ? (
+          <CarPlaceholder
+            image={carData.carModels[2]}
+            customClass="md:h-[16rem] md:w-[30rem] top-[40%] left-[85%] hidden lg:block"
+          />
+        ) : (
+          <></>
+        )}
+
+        <BenchmarkRow top={true}>
+          {Object.entries(carData.carBenchmarks)
+            .slice(0, 3)
+            .map(([keyframes, value], index) => (
+              <CarBenchmarks
+                key={index}
+                title={keyframes}
+                benchMark={value as any}
+              />
+            ))}
+        </BenchmarkRow>
+
+        <BenchmarkRow top={false}>
+          {Object.entries(carData.carBenchmarks)
+            .slice(3, 6)
+            .map(([key, value], index) => (
+              <CarBenchmarks key={index} title={key} benchMark={value as any} />
+            ))}
+        </BenchmarkRow>
+
+        <CarColorSection>
+          {carData.carColors.map((color: any, index: any) => (
+            <CarColor key={index} color={color.hex} customClass={color.class} />
+          ))}
+        </CarColorSection>
+      </BenchMarkSection>
+
+      <VideoPlayer video={carData.video} />
+
+      {carData.wideImages.map((sectionData: any, index: any) => {
+        if (carData.wideImages.length == 0) {
+          return <></>;
+        }
+        return (
+          <ColorContext.Provider key={index} value={carData.primaryColor}>
+            <WideImage
+              key={index}
+              title={sectionData.title}
+              image={sectionData.image}
+            />
+          </ColorContext.Provider>
+        );
+      })}
+
+      {carData.cropedWideImages.map((sectionData: any, index: any) => {
+        if (carData.cropedWideImages.length == 0) {
+          return <></>;
+        }
+
+        return (
+          <ColorContext.Provider key={index} value={carData.primaryColor}>
+            <SlicedWideImage
+              title={sectionData.title}
+              images={sectionData.images}
+            >
+              <p className={`${sectionData.text.class}`}>
+                {sectionData.text.text}
+              </p>
+            </SlicedWideImage>
+          </ColorContext.Provider>
+        );
+      })}
+
+      <InformativeGallery title={carData.informativeGallery.title}>
+        {carData.informativeGallery.informativeCards.map(
+          (card: any, index: any) => (
+            <InformativeCard key={index} image={card.image} text={card.text} />
+          )
+        )}
+      </InformativeGallery>
+
+      <SinglePictureSection
+        title={carData.singleImageSection.title}
+        image={carData.singleImageSection.image}
+      />
+
+      <CarGallerySection images={carData.carGallery} />
+
+      <SpecsSection>
+        {Object.entries(carData.carSpecs).map(([key, value], index) => (
+          <ColorContext.Provider key={index} value={carData.primaryColor}>
+            <CarSpects key={index} title={key} specs={value as any} />
+          </ColorContext.Provider>
+        ))}
+      </SpecsSection>
+
+      <Viewer3D carData={carData} />
+
+      <ColorContext.Provider value={carData.primaryColor}>
+        <ContactForm />
+      </ColorContext.Provider>
+
+      <Footer />
+    </>
+  );
+}
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import clsx from "clsx";
 import { usePrimaryColor } from "./color-context";
-import { Menu, X } from "lucide-react";
+import { Menu, X, ChevronDown } from "lucide-react";
 
 function NavBar() {
   const [toggledNav, setToggledNav] = useState<boolean>(false);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
   const [scrollPosition, setScrollPosition] = useState(0);
+
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
@@ -36,8 +38,39 @@ function NavBar() {
           <h1 className="text-xl font-bold">JETOUR</h1>
           <p className="text-md font-normal">--Drive Your Future--</p>
         </figure>
+
+        {/* Desktop Navigation */}
         <div className="lg:flex flex-row gap-10 text-xs text-white hidden">
-          <Link href={"/"}>MODELOS</Link>
+          {/* Dropdown for MODELOS */}
+          <div className="relative group">
+            <button
+              className="flex items-center gap-1 hover:text-gray-300"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              MODELOS
+              <ChevronDown size={16} />
+            </button>
+            {dropdownOpen && (
+              <div className="absolute top-full left-0 mt-2 w-48 bg-customBlack text-white shadow-lg rounded-lg flex flex-col">
+                <Link
+                  href={"/landing/dashing"}
+                  className="p-2 hover:bg-gray-700"
+                >
+                  Dashing
+                </Link>
+                <Link href={"/landing/t2"} className="p-2 hover:bg-gray-700">
+                  T2
+                </Link>
+                <Link href={"/landing/x50"} className="p-2 hover:bg-gray-700">
+                  X50
+                </Link>
+                <Link href={"/landing/x70"} className="p-2 hover:bg-gray-700">
+                  X70 PLUS
+                </Link>
+              </div>
+            )}
+          </div>
+
           <Link href={"/"}>SOLICITA UNA COTIZACIÓN</Link>
           <Link href={"/"}>POSTVENTA</Link>
           <Link href={"/"}>NOSOTROS</Link>
@@ -45,6 +78,8 @@ function NavBar() {
           <Link href={"/"}>BLOG</Link>
           <Link href={"/"}>CONTÁCTANOS</Link>
         </div>
+
+        {/* Mobile Menu Toggle */}
         <Menu
           onClick={() => {
             setToggledNav(!toggledNav);
@@ -62,9 +97,49 @@ function NavBar() {
             onClick={() => setToggledNav(false)}
           />
           <div className="flex flex-col gap-6 text-xl text-center">
-            <Link href={"/"} onClick={() => setToggledNav(false)}>
-              MODELOS
-            </Link>
+            {/* Dropdown for MODELOS in Mobile */}
+            <div className="relative">
+              <button
+                className="flex items-center gap-1 justify-center"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                MODELOS
+                <ChevronDown size={20} />
+              </button>
+              {dropdownOpen && (
+                <div className="mt-2 flex flex-col bg-customBlack text-white rounded-lg">
+                  <Link
+                    href={"/landing/dashing"}
+                    onClick={() => setToggledNav(false)}
+                    className="p-2 hover:bg-gray-700"
+                  >
+                    Dashing
+                  </Link>
+                  <Link
+                    href={"/landing/t2"}
+                    onClick={() => setToggledNav(false)}
+                    className="p-2 hover:bg-gray-700"
+                  >
+                    T2
+                  </Link>
+                  <Link
+                    href={"/landing/x50"}
+                    onClick={() => setToggledNav(false)}
+                    className="p-2 hover:bg-gray-700"
+                  >
+                    X50
+                  </Link>
+                  <Link
+                    href={"/landing/x70"}
+                    onClick={() => setToggledNav(false)}
+                    className="p-2 hover:bg-gray-700"
+                  >
+                    X70 PLUS
+                  </Link>
+                </div>
+              )}
+            </div>
+
             <Link href={"/"} onClick={() => setToggledNav(false)}>
               SOLICITA UNA COTIZACIÓN
             </Link>
