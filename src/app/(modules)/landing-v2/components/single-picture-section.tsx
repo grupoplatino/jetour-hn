73d14b0f7ff9255effa95ea32c6fb83600224975
keven-bardales <keven.bardales@gmail.com>
2025-04-06 08:44:02 -0600

    fix: padding

--- src/app/(modules)/landing-v2/components/single-picture-section.tsx
+++ src/app/(modules)/landing-v2/components/single-picture-section.tsx
@@ -10,7 +10,7 @@ interface SinglePictureSectionProps {
 
 const SinglePictureSection: React.FC<SinglePictureSectionProps> = ({ title, image, altText = 'Vehículo Jetour' }) => {
   return (
-    <section className="relative min-h-screen w-full flex flex-col justify-start py-28 items-center lg:px-0 px-12">
+    <section className="relative min-h-screen w-full flex flex-col justify-start py-16 items-center lg:px-0 px-12">
       <h1 className="font-bold text-4xl uppercase text-center mb-8">{title}</h1>
       <div className="w-full lg:w-[60%] h-full">
         <Image className="w-full h-auto object-cover" src={image} alt={altText} priority />
