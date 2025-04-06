e7f47413360976336c66ddc84dced21317c1508c
keven-bardales <keven.bardales@gmail.com>
2025-04-06 09:03:49 -0600

    fix: videeo url

--- src/app/(modules)/landing-v2/components/car-video-section.tsx
+++ src/app/(modules)/landing-v2/components/car-video-section.tsx
@@ -7,10 +7,7 @@ export default function VideoSection({ videoUrl }: VideoSectionProps) {
   return (
     <section className="relative w-full min-h-[500px] flex justify-center items-center overflow-hidden py-16 px-40">
       <div className="max-w-full max-h-full w-full">
-        <video autoPlay muted loop controls className="w-full h-[550px] object-cover">
-          <source src={videoUrl} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <video src={videoUrl} typeof="video/mp4" controls muted loop autoPlay className="w-full h-[550px] object-cover"></video>
       </div>
     </section>
   );
