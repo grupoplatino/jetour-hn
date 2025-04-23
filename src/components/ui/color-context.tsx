f4f0d904afe954b30717803f8cc5f93391c3f9da
keven-bardales <keven.bardales@gmail.com>
2025-04-23 00:51:51 -0600

    fix; remove unused component

--- src/components/ui/color-context.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-const ColorContext = createContext<string | undefined>("#00a3b4");
-
-export const usePrimaryColor = () => useContext(ColorContext);
-
-export default ColorContext;
