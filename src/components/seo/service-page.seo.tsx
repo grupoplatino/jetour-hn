e70e2dd29269bfda5aa437590d02d797e5123d5c
keven-bardales <keven.bardales@gmail.com>
2025-04-26 11:50:41 -0600

    fix: postventa

--- src/components/seo/service-page.seo.tsx
+++ src/components/seo/service-page.seo.tsx
@@ -27,7 +27,7 @@ export function generateServiceMetadata(): Metadata {
       url: 'https://www.jetourhn.com/landing/post-venta',
       images: [
         {
-          url: '/post-venta/hero_background.webp',
+          url: 'https://www.jetourhn.com/postventa/hero_background.jpg',
           width: 1200,
           height: 630,
           alt: 'Servicio Técnico Jetour Honduras'
