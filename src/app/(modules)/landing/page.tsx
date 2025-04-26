b4c0e65e6481213321b09c16b6bd947349375e2c
keven-bardales <keven.bardales@gmail.com>
2025-04-26 10:23:12 -0600

    fix: remove logo main

--- src/app/(modules)/landing/page.tsx
+++ src/app/(modules)/landing/page.tsx
@@ -66,7 +66,7 @@ export default function LandingPage() {
       <section className="mt-[10px] relative">
         <Image className="w-full" alt="Logo home jetour" height={800} width={800} unoptimized src={'/landing/main_hero_background.webp'} />
 
-        <div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[18rem] ">
+        {/* <div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[18rem] ">
           <div className="flex flex-col">
             <h1 className="font-bold text-6xl text-center text-black">Bienvenido a</h1>
             <Image
@@ -77,8 +77,7 @@ export default function LandingPage() {
               src={'/landing/jetour_logo_black_drive_your_future.png'}
             />
           </div>
-          {/* <h2 className="font-semibold text-3xl text-center text-black -mt-5">Diseñado para moverte, creado para inspirarte</h2> */}
-        </div>
+        </div> */}
       </section>
 
       <VideoHeroSection
