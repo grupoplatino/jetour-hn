f7e7fd4dedb7148c8786b85e571224e2f18fdd59
keven-bardales <keven.bardales@gmail.com>
2025-05-07 09:20:40 -0600

    fix: phone bg

--- src/app/(modules)/landing/page.tsx
+++ src/app/(modules)/landing/page.tsx
@@ -1,29 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import dynamic from "next/dynamic";
-import WhatsAppButton from "./components/whats-app-button";
+import dynamic from 'next/dynamic';
+import WhatsAppButton from './components/whats-app-button';
 
-import { getVehicleById, vehiclesData } from "./data/vehicles-constant";
+import { getVehicleById, vehiclesData } from './data/vehicles-constant';
 
-const VideoHeroSection = dynamic(
-  () => import("./components/video-hero-section")
-);
+const VideoHeroSection = dynamic(() => import('./components/video-hero-section'));
 
-import Image from "next/image";
+import Image from 'next/image';
 
-import { Metadata } from "next";
-import CarModelsGallery from "./components/car-model-gallery";
-import ContactForm from "./components/contact-form";
-import {
-  generateLandingMetadata,
-  LandingSEO,
-} from "@/components/seo/landing-seo";
+import { Metadata } from 'next';
+import CarModelsGallery from './components/car-model-gallery';
+import ContactForm from './components/contact-form';
+import { generateLandingMetadata, LandingSEO } from '@/components/seo/landing-seo';
 
 export const metadata: Metadata = generateLandingMetadata();
 
 export default function LandingPage() {
-  const carTheme = "orange"; // Cambia esto según el vehículo seleccionado
+  const carTheme = 'orange'; // Cambia esto según el vehículo seleccionado
 
-  const landingCar = getVehicleById("t2");
+  const landingCar = getVehicleById('t2');
   if (!landingCar) {
     return <div>Car not found</div>;
   }
@@ -35,15 +30,8 @@ export default function LandingPage() {
       <WhatsAppButton />
       {/* <TestDriveButton carTheme={carTheme} /> */}
 
-      <section className="mt-[10px] relative">
-        <Image
-          className="w-full"
-          alt="Logo home jetour"
-          height={800}
-          width={800}
-          unoptimized
-          src={"/landing/main_hero_background.webp"}
-        />
+      <section className="mt-[90px] lg:mt-[10px] relative">
+        <Image className="w-full" alt="Logo home jetour" height={800} width={800} unoptimized src={'/landing/main_hero_background.webp'} />
 
         {/* <div className="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[18rem] ">
           <div className="flex flex-col">
@@ -63,33 +51,30 @@ export default function LandingPage() {
         extraClassName=""
         sections={[
           {
-            backgroundImage: "/landing/videosection1_background.webp",
-            logo: "/landing/videosection1_car_logo.webp",
-            title: "PIENSA EN LA AVENTURA",
-            subtitle: "",
-            themeKey: "orange",
+            backgroundImage: '/landing/videosection1_background.webp',
+            logo: '/landing/videosection1_car_logo.webp',
+            title: 'PIENSA EN LA AVENTURA',
+            subtitle: '',
+            themeKey: 'orange',
             videos: {
-              leftVideo: "/landing/videosection1_mainvideo.mp4",
-            },
+              leftVideo: '/landing/videosection1_mainvideo.mp4'
+            }
           },
           {
-            imageLeftExtraClassName: "md:-ml-[0] lg:-ml-[400px]",
-            backgroundImage: "/landing/videosection2_background.webp",
-            logo: "/landing/videosection2_car_logo.webp",
-            title: "DISEÑADA PARA SATISFACER A LOS MAS EXIGENTES",
-            subtitle: "",
-            themeKey: "turquoise",
+            imageLeftExtraClassName: 'md:-ml-[0] lg:-ml-[400px]',
+            backgroundImage: '/landing/videosection2_background.webp',
+            logo: '/landing/videosection2_car_logo.webp',
+            title: 'DISEÑADA PARA SATISFACER A LOS MAS EXIGENTES',
+            subtitle: '',
+            themeKey: 'turquoise',
             videos: {
-              leftVideo: "/landing/videosection2_mainvideo.mp4",
-            },
-          },
+              leftVideo: '/landing/videosection2_mainvideo.mp4'
+            }
+          }
         ]}
       />
 
-      <CarModelsGallery
-        models={Object.values(vehiclesData)}
-        themeKey="orange"
-      />
+      <CarModelsGallery models={Object.values(vehiclesData)} themeKey="orange" />
 
       <ContactForm themeKey="orange" />
     </>
